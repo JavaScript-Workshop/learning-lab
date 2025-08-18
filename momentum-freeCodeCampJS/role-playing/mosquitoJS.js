22274856e391ba403dedc9cf6e245c502de0b99f
Davonne Vigil <davonnejv@gmail.com>
2025-08-17 21:30:08 -0500

    feature: questions 9-11 on freeCodeCamp JS, create variables, arrays, and divs in html with ids

--- momentum-freeCodeCampJS/role-playing/mosquitoJS.js
+++ momentum-freeCodeCampJS/role-playing/mosquitoJS.js
@@ -12,11 +12,16 @@
 
 console.log("Hello World");
 
-//Step 4 and 5, 6, 7, 8
+//Step 4 and 5, 6, 7, 8, 9, 10, 11
 //declare a variable, assign value to 0
 //more variables...!
+//question 11, create four divs in html with ids
 
 let myXp = 0;
 let myHealth = 100;
 let gold = 50;
 let currentWeaponIndex = 0;
+
+let karating;
+let monsterHealth;
+let inventory = ["log"];
