f8b1d45e93f20bf97b5d78d74218bfaddc78826b
Davonne Vigil <davonnejv@gmail.com>
2025-08-17 23:25:12 -0500

    feature: steps 12-16, more variables, querySelector, styles, and notes

--- momentum-freeCodeCampJS/role-playing/finishHim.js
+++ momentum-freeCodeCampJS/role-playing/finishHim.js
@@ -12,10 +12,11 @@
 
 console.log("Hello World");
 
-//Step 4 and 5, 6, 7, 8, 9, 10, 11
+//Step 4 and 5, 6, 7, 8, 9, 10,
 //declare a variable, assign value to 0
-//more variables...!
+//more variables...! oh you know these
 //question 11, create four divs in html with ids
+//question 12 and add the text for the divs, health, gold, XP
 
 let myXp = 0;
 let myHealth = 100;
@@ -25,3 +26,9 @@ let currentWeaponIndex = 0;
 let karating;
 let monsterHealth;
 let inventory = ["log"];
+
+//question 15
+//create a querySelector for button1
+//nice you already did question 16, have js script at the end of the body tag
+
+let button1 = document.querySelector("#button1");
