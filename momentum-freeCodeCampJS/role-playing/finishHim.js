f34e8c8c244ffe8bd25db39a2c282db419622e33
Davonne Vigil <davonnejv@gmail.com>
2025-08-31 21:56:41 -0500

    steps 40-40, adding functions, updating inner text and buttons to our Code Kai vs Miyagi Dev Fan Code Role playing game, in freeCodeCamp

--- momentum-freeCodeCampJS/role-playing/finishHim.js
+++ momentum-freeCodeCampJS/role-playing/finishHim.js
@@ -45,17 +45,33 @@ const codeStats = document.querySelector("#codeStats");
 const codeName = document.querySelector("codeName");
 const codeHealthText = document.querySelector("codeHealth");
 
-//go to the store
+const goToCamp = () => {
+  button1.innerText = "Go to la casa!!";
+  button1.onclick = goHome;
+  button2.innerText = "Go to camp";
+  button2.onclick = goToTrainingCamp;
+  button3.innerText = "Fight Lex Luther";
+  button3.onclick = goToCamp;
+
+  text.innerText =
+    "You have entered into the training camp!! Do you see a button that says Go to la casa?";
+};
 
 const goHome = () => {
   button1.innerText = "Comprar diez health boost!! 10 kata";
+  button1.onclick = buyBoost;
   button2.innerText = "Buy self defense boost!! 30 kata";
-  button3.innerText = "Go to town square";
+  button2.onclick = buyDefense;
+  button3.innerText = "Go to training camp";
+  button3.onclick = goToCamp;
+
+  text.innerText = "You have entered la casa!!";
 };
 
+//functions
 //go to the dojo
-const goToDojo = () => {
-  console.log("Go to the dojo");
+const goToTrainingCamp = () => {
+  console.log("Go to training camp");
 };
 
 //fight lex luther
@@ -63,6 +79,10 @@ const fightLexLuther = () => {
   console.log("I dare you.");
 };
 
+const buyBoost = () => {};
+
+const buyDefense = () => {};
+
 button1.onclick = goHome;
-button2.onclick = goToDojo;
+button2.onclick = goToTrainingCamp;
 button3.onclick = fightLexLuther;
